464a7830ac0ee28f0f23d3033838b30d43202a01
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-03-20 10:22:16 -0700

    Add PyMOL.app v2.1.0

---
+++ Casks/pymol.rb
@@ -0,0 +1,10 @@
+cask 'pymol' do
+  version '2.1.0'
+  sha256 '17fb1510281966b8363d2de4bfabc765635697770bac0443c76f7cd22ed02bd6'
+
+  url "https://pymol.org/installers/PyMOL-2.1.0_0-MacOS.dmg"
+  name 'PyMOL'
+  homepage 'https://pymol.org/2/'
+
+  app 'PyMOL.app'
+end
